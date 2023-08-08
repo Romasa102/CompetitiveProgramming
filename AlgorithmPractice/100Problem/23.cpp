a4ff6284e3cfb21a1dd8827cf015dba94b483faf
Romasa102 <masa.robot123@gmail.com>
2023-08-08 15:04:59 +0800

    clion on Mac

--- AlgorithmPractice/100Problem/23.cpp
+++ AlgorithmPractice/100Problem/23.cpp
@@ -1,31 +1,9 @@
-#include <cassert>
-#include <cctype>
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
-#include <algorithm>
-#include <bitset>
-#include <deque>
-#include <functional>
-#include <iostream>
-#include <iomanip>
-#include <limits>
-#include <list>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
-#include <string>
-#include <tuple>
-#include <utility>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
-#define repp(i, c, n) for (ll i = c; i < (n); ++i)
 using ll = long long;
 #define rep(i,n) for(ll i = 0; i < (n); ++i)
 using P = pair<ll,ll>;
 int main(){
-    
+    ll N;
+
 }
