7a6ec8cea663456e4cc693fbb1ad337143cab0a9
Romasa102 <masa.robot123@gmail.com>
2023-08-10 00:19:00 +0800

    Update 27.cpp

--- AlgorithmPractice/100Problem/27.cpp
+++ AlgorithmPractice/100Problem/27.cpp
@@ -4,27 +4,35 @@ using ll = long long;
 #define rep(i,n) for(ll i = 0; i < (n); ++i)
 using P = pair<ll,ll>;
 ll m,n;
+ll maps[100][100];
 ll longestP = 0;
-void dfs(ll i,ll j, ll count,ll mapo[100][100]){
-    cout << i << " " << j << "    ";
-    mapo[i][j] = 0;
+bool visited[100][100];
+void dfs(ll i,ll j, ll count){
+    if(visited[i][j])return;
     longestP = max(longestP,count);
-    if(i+1<m && mapo[i+1][j] == 1){
-        dfs(i+1,j,count+1,mapo);
+    if(i+1<m && maps[i+1][j] == 1){
+        visited[i][j] = true;
+        dfs(i+1,j,count+1);
+        visited[i][j] = false;
     }
-    if(i-1>=0 && mapo[i-1][j] == 1){
-        dfs(i-1,j,count+1,mapo);
+    if(i-1>=0 && maps[i-1][j] == 1){
+        visited[i][j] = true;
+        dfs(i-1,j,count+1);
+        visited[i][j] = false;
     }
-    if(j+1<n && mapo[i][j+1] == 1){
-        dfs(i,j+1,count+1,mapo);
+    if(j+1<n && maps[i][j+1] == 1){
+        visited[i][j] = true;
+        dfs(i,j+1,count+1);
+        visited[i][j] = false;
     }
-    if(j-1>=0 && mapo[i][j-1] == 1) {
-        dfs(i, j - 1, count + 1, mapo);
+    if(j-1>=0 && maps[i][j-1] == 1){
+        visited[i][j] = true;
+        dfs(i,j-1,count+1);
+        visited[i][j] = false;
     }
 }
 int main(){
     cin >> m >> n;
-    ll maps[100][100];
     rep(i,n){
         rep(j,m){
             cin >> maps[j][i];
@@ -33,8 +41,12 @@ int main(){
     rep(j,n){
         rep(i,m){
             if(maps[i][j] == 0)continue;
-            dfs(i,j,1,maps);
-            cout << endl;
+            rep(i,m){
+                rep(j,n){
+                    visited[i][j] = false;
+                }
+            }
+            dfs(i,j,1);
         }
     }
     cout << longestP << endl;
