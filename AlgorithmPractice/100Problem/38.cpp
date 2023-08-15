083e9df3a2cc421b751b08fb3869b828f4b32a07
Romasa102 <masa.robot123@gmail.com>
2023-08-15 19:54:06 +0800

    38

--- AlgorithmPractice/100Problem/38.cpp
+++ AlgorithmPractice/100Problem/38.cpp
@@ -4,5 +4,18 @@ using ll = long long;
 #define rep(i,n) for(ll i = 0; i < (n); ++i)
 using P = pair<ll,ll>;
 int main(){
-    
+    ll N;
+    cin >> N;
+    rep(i,N){
+        string A,B;
+        cin >> A >> B;
+        ll cont[A.size()][B.size()];
+        rep(j,A.size()){
+            rep(k,B.size()){
+                if(A[j] == B[k]){
+                    cont[j][k] =
+                }
+            }
+        }
+    }
 }
