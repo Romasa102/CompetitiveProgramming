baf35e50db134cdc66bd8601a291b369d0b38be8
Romasa102 <masa.robot123@gmail.com>
2023-09-21 22:58:06 +0800

    JOI

--- JOI/6/NightMarket.cpp
+++ JOI/6/NightMarket.cpp
@@ -11,5 +11,26 @@ int main(){
     rep(i,N){
         cin >> A[i] >> B[i];
     }
-    
+    ll dp[N+1][T+1];
+    rep(i,N+1){
+        rep(j,T+1){
+            dp[i][j] = 0;
+        }
+    }
+    repp(i,1,N+1){
+        repp(j,1,T+1){
+            if(j < B[i-1] ||  (j > S && j-B[i-1] < S)){
+                dp[i][j] = dp[i-1][j];
+            }else{
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - B[i - 1]] + A[i - 1]);
+            }
+        }
+    }
+    ll maxN = 0;
+    rep(i,N+1){
+        rep(j,T+1){
+            maxN = max(maxN,dp[i][j]);
+        }
+    }
+    cout << maxN << endl;
 }
