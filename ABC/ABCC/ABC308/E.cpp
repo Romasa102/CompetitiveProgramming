d4c02c393701b80ac1aa35fba3663b6e3b8501c2
Romasa102 <masa.robot123@gmail.com>
2023-09-30 18:02:21 +0800

    green

--- ABC/ABCC/ABC308/E.cpp
+++ ABC/ABCC/ABC308/E.cpp
@@ -11,20 +11,51 @@ int main(){
     rep(i,N)cin >> A[i];
     string S;
     cin >> S;
-    ll dp[N][3][8];//0
-    rep(i,N)rep(j,3)rep(k,3)dp[i][j][k]=0;
+    ll dp[N+1][3][8];//0
+    rep(i,N+1)rep(j,3)rep(k,8)dp[i][j][k]=0;
     repp(i,1,N+1){
         if(S[i-1] == 'M'){
-                dp[i][0][1<<A[i]] = dp[i-1][0][1<<A[i]];
-        }else if(S[i-1] == 'E'){
-            for(int j:{1,2,4}){
-                dp[i][1][j&(1<<A[i])] += dp[i][1][j];
+            rep(j,8){
+                dp[i][0][j] = dp[i-1][0][j];
             }
+            dp[i][0][1<<A[i-1]] = dp[i-1][0][1<<A[i-1]]+1;
         }else{
-            for(int j:{1,2,3,4,5,6}){
-                dp[i][2][j&(1<<A[i])] += dp[i][1][j];
+            rep(j,8){
+                dp[i][0][j] = dp[i-1][0][j];
             }
         }
+        if(S[i-1] == 'E') {
+            rep(j,8){
+                dp[i][1][j] = dp[i-1][1][j];
+            }
+            for (int j: {1, 2, 4}) {
+                cout << (j|(1<<A[i-1])) << endl;
+                dp[i][1][j | (1 << A[i-1])] += dp[i-1][0][j];
+            }
+        }else{
+            rep(j,8){
+                dp[i][1][j] = dp[i-1][1][j];
+            }
+        }
+        if(S[i-1] == 'X'){
+            rep(j,8){
+                dp[i][2][j] = dp[i-1][2][j];
+            }
+            for(int j:{1,2,3,4,5,6}){
+                dp[i][2][j|(1<<A[i-1])] += dp[i-1][1][j];
+            }
+        }else{
+            rep(j,8){
+                dp[i][2][j] = dp[i-1][2][j];
+            }
+        }
+    }
+    rep(i,N+1){
+        rep(j,3){
+            rep(k,8){
+                cout << dp[i][j][k] << " " ;
+            }cout << endl;
+        }cout << endl;
     }
     ll ans = 0;
     ans += dp[N][2][1] * 1;
