77045a155f8c5d8eb5ca817cb7b5567d9f6899d1
Romasa102 <masa.robot123@gmail.com>
2023-10-23 20:15:44 +0800

    DP

--- AlgorithmPractice/AtCoderTypical/11.cpp
+++ AlgorithmPractice/AtCoderTypical/11.cpp
@@ -8,8 +8,36 @@ int main(){
     ll N;
     cin >> N;
     ll D[N],C[N],S[N];
+    ll dp[6000][N+10];
     rep(i,N){
         cin >> D[i] >> C[i] >> S[i];
     }
-    
+    ll ans = 0;
+    rep(i,6000){
+        rep(j,N+1){
+            dp[i][j]=0;
+        }
+    }
+    rep(i,5000){
+        rep(j,N){
+            if(i+C[j] <= D[j]){
+                dp[i+C[j]][j+1] = max(dp[i+C[j]][j+1],dp[i][j]+S[j]);
+            }
+            dp[i+1][j]=max(dp[i+1][j],dp[i][j]);
+            if(j>0){
+                dp[i+1][j]=max(dp[i+1][j],dp[i+1][j-1]);
+            }
+        }
+    }
+    rep(i,5000){
+        rep(j,N+1){
+            ans = max(ans,dp[i][j]);
+        }
+    }
+    rep(i,20){
+        rep(j,N+1){
+            cout << dp[i][j] << " ";
+        }cout << endl;
+    }
+    cout << ans << endl;
 }
