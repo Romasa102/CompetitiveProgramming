35c4d96332dbed3276ce567b1149fcf4ee71e0bf
Romasa102 <masa.robot123@gmail.com>
2023-11-24 00:05:33 +0800

    solved 2 JOI

--- JOI/6/CoinCollecting2.cpp
+++ JOI/6/CoinCollecting2.cpp
@@ -11,12 +11,11 @@ int main(){
     rep(i,H)cin >> S[i];
     ll horizontS[H];//Aoi's
     vector<ll> verticalS(W);//Aoi's
-    ll total = 0;//Aoi's
+    ll total = H*W;//Aoi's
     rep(i,H){
         ll cur =0;
         rep(j,W){
             if(S[i][j]=='#'){
-                total++;
                 cur++;
             }
         }
@@ -31,8 +30,32 @@ int main(){
         }
         verticalS[i]=cur;
     }
+    ll maxA = 0;
+    vector<ll> cash;
     rep(i,H){
-        vector<ll> cash = verticalS;
-        
+        cash = verticalS;
+        //let's assume that Aoi select the line i and flipped it.
+        rep(j,W){
+            if(S[i][j]=='#'){
+                cash[j]--;
+            }else{
+                cash[j]++;
+            }
+        }
+        ll maxV = 0;
+        ll minIndex=0;
+        rep(j,W){
+            if(cash[j]>maxV) {
+                maxV=cash[j];
+                minIndex=j;
+            }
+        }
+        ll totalC = 0;
+        cash[minIndex] = H-cash[minIndex];
+        rep(j,W){
+            totalC+=cash[j];
+        }
+        maxA = max(maxA,totalC);
     }
+    cout << maxA << " " << total-maxA << endl;
 }
--- JOI/6/Relay.cpp
+++ JOI/6/Relay.cpp
@@ -4,57 +4,32 @@ using ll = long long;
 #define rep(i,n) for(ll i = 0; i < (n); ++i)
 #define repp(i,o,n) for(ll i = o; i < (n); ++i)
 using P = pair<ll,ll>;
+bool compare(P a, P b) {
+    return a.second < b.second;
+}
 int main(){
     ll N;
     cin >> N;
-    ll A[N];
-    ll B[N];
-    ll smallest = 1LL<<40;
-    ll smallestInd;
-    ll smallestB;
-    vector<ll> BGROUP;
-    ll ans = 0;
+    vector<P> AB(N);
+    vector<P> TOTALB(N);
     rep(i,N){
-        cin >> A[i] >> B[i];
-        if(smallest>(A[i]+B[i])){
-            smallest = A[i]+B[i];
-            smallestB = B[i];
-            smallestInd = i;
-        }else if(smallest==A[i]+B[i]){
-            if(B[i] < smallestB){
-                smallestB = B[i];
-                smallestInd = i;
-            }
-        }
+        cin >> AB[i].first >> AB[i].second;
+        TOTALB[i].first = AB[i].second;
+        TOTALB[i].second = AB[i].first + AB[i].second;
     }
-    ans += smallest;
-    vector<P> sum;
-    BGROUP.push_back(smallestB);
-    rep(i,N){
-        if(i==smallestInd)continue;
-        if(B[i]>smallestB){
-            sum.push_back({A[i]+B[i]-smallestB,i});
-        }else{
-            sum.push_back({A[i],i});
-        }
+    sort(AB.begin(),AB.end(),compare);
+    sort(TOTALB.begin(),TOTALB.end());
+    vector<ll> minA(N,1LL<<50);
+    vector<ll> minS(N,1LL<<50);
+    repp(i,1,N){
+        minA[i]=min(minA[i-1],AB[i-1].first);
     }
-    sort(sum.begin(),sum.end());
-    ans+=sum[0].first;
-    BGROUP.push_back(B[sum[0].second]);
-    smallestB = min(smallestB,B[sum[0].second]);
-    ans+=smallestB;
-    vector<ll> Sum2;
-    rep(i,N){
-        if(i==smallestInd||i==sum[0].second){
-            continue;
-        }
-        if(B[i] > smallestB){
-            Sum2.push_back(A[i]+B[i]-smallestB);
-        }else{
-            Sum2.push_back(A[i]);
-        }
+    for(ll i = N-2;i>=0;i--){
+        minS[i]=min(minS[i+1],TOTALB[i+1].second);
     }
-    sort(Sum2.begin(),Sum2.end());
-    ans += Sum2[0];
+    ll ans =1LL<<50;
+    repp(i,1,N-1){
+        ans = min(ans,AB[i].first+AB[i].second+minA[i]+minS[i]);
+    }
     cout << ans << endl;
 }
