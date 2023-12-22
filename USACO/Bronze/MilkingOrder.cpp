006f52463cf4d0d917bdfc6763d172c2efb7c74c
Romasa102 <masa.robot123@gmail.com>
2023-12-22 21:07:25 +0800

    deleted cmake stuff

--- USACO/Bronze/MilkingOrder.cpp
+++ USACO/Bronze/MilkingOrder.cpp
@@ -4,60 +4,38 @@ using ll = long long;
 #define rep(i,n) for(ll i = 0; i < (n); ++i)
 #define repp(i,o,n) for(ll i = o; i < (n); ++i)
 using P = pair<ll,ll>;
+ll N,M,K;
+bool check(vector<ll> pos,vector<ll> hierarchy){
+    vector<ll> cowToPos(N);
+    rep(i,pos.size()){
+        if(pos[i]!=-1){
+            cowToPos[pos[i]]=i;
+        }
+    }
+    rep(i,N){
+
+    }
+}
 int main(){
-    freopen("milkorder.in","r",stdin);
-    ll N,M,K;
     cin >> N >> M >> K;
-    ll m[M],c[K],p[K];
-    rep(i,M)cin >> m[i];
-    rep(i,K)cin >> c[i] >> p[i];
-    vector<bool> LockPos(M,false);//place that is already locked
-    vector<ll> pos(N,-1);//current state
-    rep(i,K){
-        pos[p[i]-1]=c[i];
-        rep(j,M){
-            if(c[i]==m[j]){
-                LockPos[j]=true;
-            }
-        }
-    }
-    vector<ll> counter;
-    vector<vector<ll>> rollBack(M);
+    vector<ll> m(M),c(K),p(K);
     rep(i,M){
-        if(!LockPos[i]){
-            counter.push_back(m[i]);
-        }else{
-            rollBack[i]=counter;
-            counter.clear();
-        }
+        cin >> m[i];
+        m[i]--;
     }
+    rep(i,K)cin >> c[i] >> p[i];
+    vector<ll> pos(N,-1);
     rep(i,K){
-        rep(j,M){
-            if(c[i]==m[j]){
-                if(rollBack[j].size()!=0){
-                    ll count = rollBack[j].size();
-                    for(ll k = p[i]-2; k >= 0; k--){
-                        if(count==0)break;
-                        if(pos[k]==-1){
-                            pos[k]=m[count-1];
-                            count--;
-                        }
-                    }
-                }
-            }
-        }
-    }
-    freopen("milkorder.out","w",stdout);
-    rep(i,N){
-        if(pos[i]==1){
-            cout << i+1 << endl;
-            return 0;
-        }
+        pos[p[i]-1]=c[i]-1;
     }
     rep(i,N){
+        vector<ll> cash = pos;
         if(pos[i]==-1){
-            cout << i+1 << endl;
-            return 0;
+            pos[i]=0;
+            if(check(cash,m)){
+                cout << i+1 << endl;
+                return 0;
+            }
         }
     }
 }
